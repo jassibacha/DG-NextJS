e928653d6fd802097850af529630c1e3816bf40d
Jassi Bacha <klikster@gmail.com>
2024-01-03 18:35:36 -0800

    Clone 08 to 09 and update the dynamic route for todos/id

---
+++ next09-middleware/src/app/api/todos/route.ts
@@ -0,0 +1,85 @@
+import { NextResponse } from 'next/server'
+
+const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+const API_KEY: string = process.env.DATA_API_KEY as string
+
+/**
+ * Fetches todos from the data source URL and returns them as a JSON response.
+ * @returns {Promise<NextResponse>} A promise that resolves with a JSON response containing the todos.
+ */
+export async function GET(): Promise<NextResponse> {
+    // Fetch todos from the data source URL
+    const response = await fetch(DATA_SOURCE_URL)
+
+    // Parse the response body as an array of todos
+    const todos: Todo[] = await response.json()
+
+    // Return a JSON response containing the todos
+    return NextResponse.json(todos)
+}
+
+export async function DELETE(request: Request) {
+    const { id }: Partial<Todo> = await request.json()
+
+    if (!id) {
+        return NextResponse.json({ error: 'Missing id' }, { status: 400 })
+    }
+
+    const response = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        }
+    })
+
+    return NextResponse.json({ "message": `Todo ${id} deleted` })
+
+}
+export async function POST(request: Request) {
+    const { userId, title }: Partial<Todo> = await request.json()
+
+    if (!userId || !title) {
+        return NextResponse.json({ error: 'Missing required data' }, { status: 400 })
+    }
+
+    const response = await fetch(DATA_SOURCE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        },
+        body: JSON.stringify({ 
+            userId, title, completed: false 
+        })
+    })
+
+    const newTodo: Todo = await response.json()
+
+    return NextResponse.json(newTodo)
+
+}
+export async function PUT(request: Request) {
+    const { userId, id, title, completed }: Todo = await request.json()
+
+    if (!userId || !id || !title || typeof completed !== 'boolean') {
+        return NextResponse.json({ error: 'Missing required data' }, { status: 400 })
+    }
+
+    const response = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        },
+        body: JSON.stringify({ 
+            userId, title, completed
+        })
+    })
+
+    const updatedTodo: Todo = await response.json()
+
+    return NextResponse.json(updatedTodo)
+
+}
