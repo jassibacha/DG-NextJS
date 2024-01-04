8458ad64ae596d61552092ae2ee6f017f7756f1f
Jassi Bacha <klikster@gmail.com>
2024-01-04 12:05:33 -0800

    Next11 done, mutations are weird

---
+++ next11-mutations/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import Navbar from "./components/Navbar"
+
+
+export const metadata: Metadata = {
+  title: 'Next Todos',
+  description: 'Created for practice',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        <main className="mx-auto max-w-xl p-4 bg-stone-200 min-h-screen">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
